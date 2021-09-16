1ecbbfc65e2a42de42ae8379b4ae7966729d947e
5cientist <luk.ku@hotmail.com>
2021-09-16 15:15:13 +0530

    added request for every name in login and signup

--- website/auth.py
+++ website/auth.py
@@ -5,12 +5,22 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods=['GET','POST'])
 def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
     return render_template("login.html")
 
 
 
 @auth.route("/sign-up", methods=['GET','POST'])
 def sign_up():
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password1 = request.form.get("password1")
+    password2 = request.form.get("password2")
+
+
+    
     return render_template("signup.html")
 
 @auth.route("/logout")
