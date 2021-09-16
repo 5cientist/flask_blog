f8686fc24237cfc4f867d5a451d0b0c282777b2d
5cientist <luk.ku@hotmail.com>
2021-09-16 15:10:05 +0530

    signup post issue solved

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint,render_template,redirect,url_for
+from flask import Blueprint,render_template,redirect,url_for,request
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET','POST'])
 def login():
     return render_template("login.html")
 
 
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=['GET','POST'])
 def sign_up():
     return render_template("signup.html")
 
