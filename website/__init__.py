a61e62f8ca33cd1196e3c651c5a5deab50f79531
5cientist <luk.ku@hotmail.com>
2021-09-16 18:08:01 +0530

    User model created

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 from os import path
-from flask_login import LoginManager
+from flask_login import LoginManager, login_manager
 
 
 db = SQLAlchemy()
@@ -24,7 +24,20 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+
+    from .models import User
+
     create_database(app)
+
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     
 
