8f990733ebd375d476c2442fe7e68f40b4ac55c9
5cientist <luk.ku@hotmail.com>
2021-09-18 00:13:55 +0530

    added current user

--- website/auth.py
+++ website/auth.py
@@ -3,7 +3,7 @@ from flask.helpers import flash
 from sqlalchemy.sql.functions import current_user, user
 from . import db
 from .models import User
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash,check_password_hash
 
 
@@ -70,7 +70,9 @@ def sign_up():
     
     return render_template("signup.html")
 
+
 @auth.route("/logout")
+@login_required
 def logout():
     logout_user()
     return redirect(url_for("views.home"))
